955f4b01e8cbb5fe9060f3c3169efce0ca39f79f
nargyanti <nargyanti@gmail.com>
2022-09-29 23:58:55 +0700

    Setup

---
+++ app/src/main/java/com/nargyanti/mycashbook/CashFlowDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.nargyanti.mycashbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CashFlowDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cash_flow_details)
+    }
+}
