c7fc94b09c13d12b7719ba440383cab7262e3c4f
nargyanti <nargyanti@gmail.com>
2022-09-30 17:12:44 +0700

    Add query for SQLite

--- app/src/main/java/com/nargyanti/mycashbook/database/DatabaseHelper.kt
+++ app/src/main/java/com/nargyanti/mycashbook/database/DatabaseHelper.kt
@@ -1,27 +1,30 @@
 package com.nargyanti.mycashbook.database
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import com.nargyanti.mycashbook.model.TransactionModel
+import com.nargyanti.mycashbook.model.UserModel
 
 class DatabaseHelper (context: Context) : SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION) {
 
     companion object {
-        private val DB_NAME = "my_cash_book"
-        private val DB_VERSION = 1
+        private const val DB_NAME = "my_cash_book"
+        private const val DB_VERSION = 1
 
-        private val TABLE_USERS = "users"
-        private val TABLE_TRANSACTIONS = "transactions"
+        private const val TABLE_USERS = "users"
+        private const val TABLE_TRANSACTIONS = "transactions"
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
         val CREATE_TABLE_USERS =
-            "CREATE TABLE IF NOT EXISTS $TABLE_USERS ('id' INTEGER, 'username' TEXT NOT NULL UNIQUE, 'password' TEXT NOT NULL, PRIMARY KEY('id' AUTOINCREMENT));"
+            "CREATE TABLE IF NOT EXISTS $TABLE_USERS ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'username' TEXT NOT NULL UNIQUE, 'password' TEXT NOT NULL);"
         val CREATE_TABLE_TRANSACTIONS =
-            "CREATE TABLE IF NOT EXISTS $TABLE_TRANSACTIONS ('id' INTEGER, 'date' TEXT NOT NULL, 'description' TEXT NOT NULL, 'category' TEXT NOT NULL, 'user_id' INTEGER, PRIMARY KEY('id' AUTOINCREMENT), FOREIGN KEY('user_id') REFERENCES 'Users'('id'));"
+            "CREATE TABLE IF NOT EXISTS $TABLE_TRANSACTIONS ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'date' TEXT NOT NULL, 'amount' INTEGER NOT NULL, 'description' TEXT NOT NULL, 'category' TEXT NOT NULL, 'user_id' INTEGER, FOREIGN KEY('user_id') REFERENCES 'Users'('id'));"
 
-        val INSERT_TABLE_TRANSACTIONS = "INSERT INTO $TABLE_USERS ('username', 'password') VALUES ('user', 'user');"
+        val INSERT_TABLE_USERS = "INSERT INTO $TABLE_USERS ('username', 'password') VALUES ('user', 'user');"
         db?.execSQL(CREATE_TABLE_USERS)
         db?.execSQL(CREATE_TABLE_TRANSACTIONS)
 
@@ -36,10 +39,11 @@ class DatabaseHelper (context: Context) : SQLiteOpenHelper(context, DB_NAME, nul
         db?.execSQL(DROP_TABLE_TRANSACTIONS)
     }
 
+    @SuppressLint("Range")
     fun getUserByUsername(_username: String) : UserModel {
         val user = UserModel()
         val db = writableDatabase
-        val selectQuery = "SELECT * FROM $TABLE_USERSUSERS WHERE 'username' = $_username"
+        val selectQuery = "SELECT * FROM $TABLE_USERS WHERE username = '$_username'"
         val cursor = db.rawQuery(selectQuery, null)
         cursor.moveToFirst()
         user.id = cursor.getInt(cursor.getColumnIndex("id"))
@@ -49,10 +53,35 @@ class DatabaseHelper (context: Context) : SQLiteOpenHelper(context, DB_NAME, nul
         return user
     }
 
+    @SuppressLint("Range")
+    fun getUserById(_id: Int) : UserModel {
+        val user = UserModel()
+        val db = writableDatabase
+        val selectQuery = "SELECT * FROM $TABLE_USERS WHERE id = $_id"
+        val cursor = db.rawQuery(selectQuery, null)
+        cursor.moveToFirst()
+        user.id = cursor.getInt(cursor.getColumnIndex("id"))
+        user.username = cursor.getString(cursor.getColumnIndex("username"))
+        user.password = cursor.getString(cursor.getColumnIndex("password"))
+        cursor.close()
+        return user
+    }
+
+    fun changePassword(user: UserModel) : Boolean {
+        val db = writableDatabase
+        val values = ContentValues()
+        values.put("id", user.id)
+        values.put("password", user.password)
+        val _success = db.update(TABLE_USERS, values, "id = ?", arrayOf(user.id.toString())).toLong()
+        db.close()
+        return (Integer.parseInt("$_success") != -1)
+    }
+
+    @SuppressLint("Range")
     fun getAllTransactionsByUserId(_userId: Int): List<TransactionModel> {
-        val transactionModel = ArrayList<TransactionModel>()
+        val transactionList = ArrayList<TransactionModel>()
         val db = writableDatabase
-        val selectQuery = "SELECT * FROM $TABLE_TRANSACTIONS WHERE 'user_id' = $_userId;"
+        val selectQuery = "SELECT * FROM $TABLE_TRANSACTIONS WHERE user_id = $_userId ORDER BY date ASC;"
         val cursor = db.rawQuery(selectQuery, null)
         if (cursor != null) {
             if (cursor.moveToFirst()) {
@@ -60,6 +89,7 @@ class DatabaseHelper (context: Context) : SQLiteOpenHelper(context, DB_NAME, nul
                     val transaction = TransactionModel()
                     transaction.id = cursor.getInt(cursor.getColumnIndex("id"))
                     transaction.date = cursor.getString(cursor.getColumnIndex("date"))
+                    transaction.amount = cursor.getInt(cursor.getColumnIndex("amount"))
                     transaction.description = cursor.getString(cursor.getColumnIndex("description"))
                     transaction.category = cursor.getString(cursor.getColumnIndex("category"))
                     transaction.user_id = cursor.getInt(cursor.getColumnIndex("user_id"))
@@ -71,47 +101,27 @@ class DatabaseHelper (context: Context) : SQLiteOpenHelper(context, DB_NAME, nul
         return transactionList
     }
 
-    fun getAllExpenseByUserId(_userId: Int): List<TransactionModel> {
-        val transactionModel = ArrayList<TransactionModel>()
+    @SuppressLint("Range")
+    fun getTransactionAmountByUserId(_category: String, _userId: Int): Int {
         val db = writableDatabase
-        val selectQuery = "SELECT * FROM $TABLE_TRANSACTIONS WHERE 'user_id' = $_userId AND 'category' = 'pengeluaran';"
+        val selectQuery = "SELECT SUM(amount) as amount FROM $TABLE_TRANSACTIONS WHERE user_id = $_userId AND category = '$_category'"
         val cursor = db.rawQuery(selectQuery, null)
-        if (cursor != null) {
-            if (cursor.moveToFirst()) {
-                do {
-                    val transaction = TransactionModel()
-                    transaction.id = cursor.getInt(cursor.getColumnIndex("id"))
-                    transaction.date = cursor.getString(cursor.getColumnIndex("date"))
-                    transaction.description = cursor.getString(cursor.getColumnIndex("description"))
-                    transaction.category = cursor.getString(cursor.getColumnIndex("category"))
-                    transaction.user_id = cursor.getInt(cursor.getColumnIndex("user_id"))
-                    transactionList.add(transaction)
-                } while (cursor.moveToNext())
-            }
-        }
+        cursor.moveToFirst()
+        val amount = cursor.getInt(cursor.getColumnIndex("amount"))
         cursor.close()
-        return transactionList
+        return amount
     }
 
-    fun getAllIncomeByUserId(_userId: Int): List<TransactionModel> {
-        val transactionModel = ArrayList<TransactionModel>()
+    fun addTransaction(transaction: TransactionModel): Boolean {
         val db = writableDatabase
-        val selectQuery = "SELECT * FROM $TABLE_TRANSACTIONS WHERE 'user_id' = $_userId AND 'category' = 'pemasukan';"
-        val cursor = db.rawQuery(selectQuery, null)
-        if (cursor != null) {
-            if (cursor.moveToFirst()) {
-                do {
-                    val transaction = TransactionModel()
-                    transaction.id = cursor.getInt(cursor.getColumnIndex("id"))
-                    transaction.date = cursor.getString(cursor.getColumnIndex("date"))
-                    transaction.description = cursor.getString(cursor.getColumnIndex("description"))
-                    transaction.category = cursor.getString(cursor.getColumnIndex("category"))
-                    transaction.user_id = cursor.getInt(cursor.getColumnIndex("user_id"))
-                    transactionList.add(transaction)
-                } while (cursor.moveToNext())
-            }
-        }
-        cursor.close()
-        return transactionList
+        val values = ContentValues()
+        values.put("date", transaction.date)
+        values.put("amount", transaction.amount)
+        values.put("description", transaction.description)
+        values.put("category", transaction.category)
+        values.put("user_id", transaction.user_id)
+        val _success = db.insert(TABLE_TRANSACTIONS, null, values)
+        db.close()
+        return (Integer.parseInt("$_success") != -1)
     }
 }
